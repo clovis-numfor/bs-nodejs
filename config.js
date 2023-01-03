d715dac8c8a22e97e87bf0dd0f4783e5c3c347ac
SAIF_ZAMAN <saif.mektek@gmail.com>
2023-01-03 23:58:46 +0600

    Added multiple env with github secret

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ dotenv.config({
 
 module.exports = {
     NODE_ENV : process.env.NODE_ENV,
-    projectName : process.env.PROJECT_NAME,
-    buildName : process.env.BUILD_NAME,
-    localIdentifier : process.env.LOCAL_IDENTIFIER,
-    username: process.env.username,
-    accessKey: process.env.access-key
+    projectName : process.env.BROWSERSTACK_PROJECT_NAME,
+    buildName : process.env.BROWSERSTACK_BUILD_NAME,
+    localIdentifier : process.env.BROWSERSTACK_LOCAL_IDENTIFIER,
+    username: process.env.BROWSERSTACK_USERNAME,
+    accessKey: process.env.BROWSERSTACK_ACCESS_KEY
 }
