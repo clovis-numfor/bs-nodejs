63aef6bd39791734e2e56ecde73ff408a130291c
SAIF_ZAMAN <saif.mektek@gmail.com>
2023-01-04 17:20:36 +0600

    Added test suites

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var webdriver = require('selenium-webdriver');
 const config =  require('./config.js');
+var chai = require('chai');
+var expect = chai.expect;
 
 // Input capabilities
 var capabilities = {
@@ -25,6 +27,7 @@ var driver = new webdriver.Builder().
 // HTTP Server should be running on 8099 port of GitHub runner
 driver.get('http://localhost:8099').then(function () {
   driver.getTitle().then(function (title) {
+    expect (title).equals("Google");
     console.log(title);
     driver.quit();
   });
