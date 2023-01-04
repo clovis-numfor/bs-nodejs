9e75902e436db6ea8793ea11f91c182fc80c5f10
SAIF_ZAMAN <saif.mektek@gmail.com>
2023-01-04 13:47:04 +0600

    Changed npm scripts

--- config.js
+++ config.js
@@ -1,10 +1,3 @@
-const dotenv = require('dotenv');
-const path = require('path');
-
-dotenv.config({
-    path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`)
-});
-
 module.exports = {
     NODE_ENV : process.env.NODE_ENV,
     projectName : process.env.BROWSERSTACK_PROJECT_NAME,
