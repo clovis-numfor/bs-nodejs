bd729d77ff28b679182e6c7b2cefdb609819b8de
SAIF_ZAMAN <saif.mektek@gmail.com>
2023-01-05 11:39:09 +0600

    Check dev

--- index.js
+++ index.js
@@ -24,11 +24,11 @@ var driver = new webdriver.Builder().
   withCapabilities(capabilities).
   build();
 
-  
+
 // HTTP Server should be running on 8099 port of GitHub runner
 driver.get('http://localhost:8099').then(function () {
   driver.getTitle().then(function (title) {
-    expect (title).equals("TEST DOCUMENT");
+    expect (title).equals("Hello");
     console.log(title);
     driver.quit();
   });
